fc71d05bb83bcfb96aef145f8759312cddf93009
Archi Lodha <archilodha1@gmail.com>
2026-03-06 09:18:34 +0530

    Implement maxProfit function to calculate stock profit

---
+++ arrays/best_time_to_buy_sell_stock.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int maxProfit(vector<int>& prices) {
+    int minPrice = INT_MAX;
+    int maxProfit = 0;
+
+    for(int i = 0; i < prices.size(); i++) {
+        if(prices[i] < minPrice) {
+            minPrice = prices[i];
+        } 
+        else {
+            int profit = prices[i] - minPrice;
+            if(profit > maxProfit) {
+                maxProfit = profit;
+            }
+        }
+    }
+
+    return maxProfit;
+}
+
+int main() {
+    vector<int> prices = {7,1,5,3,6,4};
+
+    cout << "Maximum Profit: " << maxProfit(prices);
+
+    return 0;
+}
