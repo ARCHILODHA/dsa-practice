e892354b321215616bf51da967e7977bd5f530a5
Archi Lodha <archilodha1@gmail.com>
2026-03-08 22:42:23 +0530

    Implement queue using two stacks

---
+++ queue/queue_using_two_stacks.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+stack<int> s1,s2;
+
+void enqueue(int x){
+    s1.push(x);
+}
+
+void dequeue(){
+
+    if(s2.empty()){
+        while(!s1.empty()){
+            s2.push(s1.top());
+            s1.pop();
+        }
+    }
+
+    cout<<"Removed "<<s2.top()<<endl;
+    s2.pop();
+}
+
+int main(){
+
+    enqueue(10);
+    enqueue(20);
+    enqueue(30);
+
+    dequeue();
+}
