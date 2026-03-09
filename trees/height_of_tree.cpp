4839ef55e7a2514b01ce05f36ce2e33a5e098898
Archi Lodha <archilodha1@gmail.com>
2026-03-09 10:02:16 +0530

    Implement height calculation for binary tree

---
+++ trees/height_of_tree.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+struct Node{
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val){
+        data=val;
+        left=right=NULL;
+    }
+};
+
+int height(Node* root){
+
+    if(root==NULL)
+        return 0;
+
+    return 1 + max(height(root->left),height(root->right));
+}
+
+int main(){
+
+    Node* root=new Node(1);
+    root->left=new Node(2);
+    root->right=new Node(3);
+
+    cout<<height(root);
+}
