e3e22e9253f77b1da65af0241bda3f360377e65b
Archi Lodha <archilodha1@gmail.com>
2026-03-22 09:57:14 +0530

    Implement maximum subarray function

---
+++ arrays/maximum_subarray.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int maxSubArray(vector<int>& nums) {
+    int sum = 0, maxi = INT_MIN;
+
+    for (int x : nums) {
+        sum += x;
+        maxi = max(maxi, sum);
+        if (sum < 0) sum = 0;
+    }
+    return maxi;
+}
