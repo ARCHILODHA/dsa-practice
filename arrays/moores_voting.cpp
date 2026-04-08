74eed18e1618f7e840221f7a26ad7da5987bd321
Archi Lodha <archilodha1@gmail.com>
2026-04-08 09:07:28 +0530

    Implement Moore's Voting Algorithm for majority element

---
+++ arrays/moores_voting.cpp
@@ -0,0 +1,9 @@
+int majorityElement(vector<int>& nums) {
+    int count = 0, candidate = -1;
+
+    for(int num : nums) {
+        if(count == 0) candidate = num;
+        count += (num == candidate) ? 1 : -1;
+    }
+    return candidate;
+}
