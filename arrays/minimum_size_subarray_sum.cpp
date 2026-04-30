0c729d4e29358bb10472834ecf2981100972a099
Archi Lodha <archilodha1@gmail.com>
2026-04-30 09:01:45 +0530

    Add minSubArrayLen function for subarray sum
    Implement function to find minimum size subarray sum.

---
+++ arrays/minimum_size_subarray_sum.cpp
@@ -0,0 +1,13 @@
+int minSubArrayLen(int target, vector<int>& nums) {
+    int sum = 0, left = 0, ans = INT_MAX;
+
+    for (int right = 0; right < nums.size(); right++) {
+        sum += nums[right];
+
+        while (sum >= target) {
+            ans = min(ans, right - left + 1);
+            sum -= nums[left++];
+        }
+    }
+    return ans == INT_MAX ? 0 : ans;
+}
