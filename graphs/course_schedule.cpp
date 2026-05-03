bd29fbb19d7022c9a97e4d7ef3ec18fa701a109a
Archi Lodha <archilodha1@gmail.com>
2026-05-03 09:05:48 +0530

    Implement course schedule check function

---
+++ graphs/course_schedule.cpp
@@ -0,0 +1,28 @@
+bool canFinish(int n, vector<vector<int>>& pre) {
+    vector<vector<int>> adj(n);
+    vector<int> indegree(n, 0);
+
+    for(auto &p : pre) {
+        adj[p[1]].push_back(p[0]);
+        indegree[p[0]]++;
+    }
+
+    queue<int> q;
+    for(int i=0;i<n;i++){
+        if(indegree[i]==0) q.push(i);
+    }
+
+    int count = 0;
+
+    while(!q.empty()){
+        int node = q.front(); q.pop();
+        count++;
+
+        for(auto &nbr : adj[node]){
+            if(--indegree[nbr]==0)
+                q.push(nbr);
+        }
+    }
+
+    return count == n;
+}
