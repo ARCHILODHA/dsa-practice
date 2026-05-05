678d4ed2103f31ba557506c78d37eeeed7aa5804
Archi Lodha <archilodha1@gmail.com>
2026-05-05 07:56:44 +0530

    Implement postfix expression evaluation function

---
+++ stack/evaluate_postfix.cpp
@@ -0,0 +1,17 @@
+int eval(string s) {
+    stack<int> st;
+
+    for(char c : s) {
+        if(isdigit(c)) st.push(c - '0');
+        else {
+            int b = st.top(); st.pop();
+            int a = st.top(); st.pop();
+
+            if(c == '+') st.push(a + b);
+            if(c == '-') st.push(a - b);
+            if(c == '*') st.push(a * b);
+            if(c == '/') st.push(a / b);
+        }
+    }
+    return st.top();
+}
