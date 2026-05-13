281d5416c4ecbd596c50c888787c821f8c22c16f
Archi Lodha <archilodha1@gmail.com>
2026-05-13 07:53:14 +0530

    Implement searchRange to find first and last positions

---
+++ binary-search/first_last_occurrence.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> searchRange(vector<int>& nums, int target) {
+    int first = lower_bound(nums.begin(), nums.end(), target) - nums.begin();
+    int last = upper_bound(nums.begin(), nums.end(), target) - nums.begin() - 1;
+
+    if (first >= nums.size() || nums[first] != target)
+        return {-1, -1};
+
+    return {first, last};
+}
