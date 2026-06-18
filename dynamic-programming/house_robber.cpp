2de010e20a12dc3574737646b80ab68794c35ba9
Archi Lodha <archilodha1@gmail.com>
2026-06-18 10:20:27 +0530

    Implement house robber solution in C++

---
+++ dynamic-programming/house_robber.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int prev = 0, curr = 0;
+
+        for(int num : nums) {
+            int temp = max(curr, prev + num);
+            prev = curr;
+            curr = temp;
+        }
+
+        return curr;
+    }
+};
