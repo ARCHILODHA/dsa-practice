cf855560ab3aa8cbd60834db954e53b96488b9c2
Archi Lodha <archilodha1@gmail.com>
2026-06-18 10:22:17 +0530

    Implement rod cutting solution using dynamic programming

---
+++ dynamic-programming/rod_cutting.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int cutRod(vector<int>& price, int n) {
+        vector<int> dp(n + 1, 0);
+
+        for(int i = 1; i <= n; i++)
+            for(int j = 0; j < i; j++)
+                dp[i] = max(dp[i],
+                           price[j] + dp[i-j-1]);
+
+        return dp[n];
+    }
+};
