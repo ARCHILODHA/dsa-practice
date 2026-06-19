b4790e267be4033a8c1b8933d8793b1d06636820
Archi Lodha <archilodha1@gmail.com>
2026-06-19 09:14:59 +0530

    Implement Floyd-Warshall algorithm in C++

---
+++ graphs/floyd_warshall.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void floydWarshall(vector<vector<int>>& dist) {
+    int V = dist.size();
+
+    for(int k = 0; k < V; k++)
+        for(int i = 0; i < V; i++)
+            for(int j = 0; j < V; j++)
+                if(dist[i][k] != INT_MAX && dist[k][j] != INT_MAX)
+                    dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
+}
