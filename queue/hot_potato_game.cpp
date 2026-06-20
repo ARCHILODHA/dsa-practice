372fe415666f33bad1862f18b94060a6592a8144
Archi Lodha <archilodha1@gmail.com>
2026-06-20 09:24:09 +0530

    Implement hot potato game using a queue

---
+++ queue/hot_potato_game.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int hotPotato(int n, int k) {
+    queue<int> q;
+
+    for(int i=1;i<=n;i++)
+        q.push(i);
+
+    while(q.size() > 1) {
+        for(int i=0;i<k-1;i++) {
+            q.push(q.front());
+            q.pop();
+        }
+        q.pop();
+    }
+
+    return q.front();
+}
