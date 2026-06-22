ca83a4d9ac6cbd6a4959475c8c99fd33204328a6
Archi Lodha <archilodha1@gmail.com>
2026-06-22 06:41:34 +0530

    Implement queue using two stacks

---
+++ stack/queue_using_stacks.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class MyQueue {
+    stack<int> s1, s2;
+
+public:
+    void push(int x) {
+        s1.push(x);
+    }
+
+    int pop() {
+        if(s2.empty()) {
+            while(!s1.empty()) {
+                s2.push(s1.top());
+                s1.pop();
+            }
+        }
+
+        int x = s2.top();
+        s2.pop();
+        return x;
+    }
+};
