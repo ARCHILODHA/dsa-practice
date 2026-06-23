80fca419108577b72fb547813a46d907c53577d9
Archi Lodha <archilodha1@gmail.com>
2026-06-23 07:09:59 +0530

    Create print_numbers.cpp

---
+++ recursion/print_numbers.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+void printNumbers(int n) {
+    if (n == 0) return;
+    printNumbers(n - 1);
+    cout << n << " ";
+}
+
+int main() {
+    printNumbers(10);
+    return 0;
+}
