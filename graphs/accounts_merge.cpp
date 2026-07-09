8343f9caba34cd8646d14118868fe8d03a552e45
Archi Lodha <archilodha1@gmail.com>
2026-07-09 08:37:35 +0530

    Add DSU class for accounts merging
    Implemented a basic DSU class for accounts merging.

---
+++ graphs/accounts_merge.cpp
@@ -0,0 +1,27 @@
+// Accounts Merge using DSU
+
+#include<iostream>
+using namespace std;
+
+class DSU{
+
+public:
+
+int parent[100];
+
+DSU(){
+
+for(int i=0;i<100;i++)
+parent[i]=i;
+
+}
+
+};
+
+int main(){
+
+DSU d;
+
+cout<<"Accounts Merge using Disjoint Set Union.";
+
+}
