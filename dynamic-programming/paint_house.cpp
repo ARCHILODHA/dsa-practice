9951738aa20c0888043cc69ac6ce89aede2d7276
Archi Lodha <archilodha1@gmail.com>
2026-08-01 09:22:54 +0530

    Implement minCost function for paint house problem

---
+++ dynamic-programming/paint_house.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int minCost(vector<vector<int>>& costs) {
+        if (costs.empty())
+            return 0;
+
+        for (int i = 1; i < costs.size(); i++) {
+            costs[i][0] += min(costs[i - 1][1], costs[i - 1][2]);
+            costs[i][1] += min(costs[i - 1][0], costs[i - 1][2]);
+            costs[i][2] += min(costs[i - 1][0], costs[i - 1][1]);
+        }
+
+        return min({costs.back()[0],
+                    costs.back()[1],
+                    costs.back()[2]});
+    }
+};
