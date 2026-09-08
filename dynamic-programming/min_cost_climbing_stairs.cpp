04b4fc32fbd03744d605fa38e80746325f84dd8d
Archi Lodha <archilodha1@gmail.com>
2026-09-08 14:31:46 +0530

    Add Minimum Cost Climbing Stairs solution
    Implement the Minimum Cost Climbing Stairs algorithm using dynamic programming.

---
+++ dynamic-programming/min_cost_climbing_stairs.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Minimum Cost Climbing Stairs
+// Time: O(n)
+// Space: O(1)
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<int> cost(n);
+
+    for (int &x : cost)
+        cin >> x;
+
+    int prev2 = 0;
+    int prev1 = 0;
+
+    for (int i = 2; i <= n; i++) {
+        int current = min(
+            prev1 + cost[i - 1],
+            prev2 + cost[i - 2]
+        );
+
+        prev2 = prev1;
+        prev1 = current;
+    }
+
+    cout << "Minimum cost: " << prev1 << endl;
+
+    return 0;
+}
