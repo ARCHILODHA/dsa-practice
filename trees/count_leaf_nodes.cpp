c6a968fb28c5bf745387f593efb14a5257d056b1
Archi Lodha <archilodha1@gmail.com>
2026-09-20 09:57:53 +0530

    Implement function to count leaf nodes in a tree

---
+++ trees/count_leaf_nodes.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
+
+int countLeafNodes(TreeNode* root) {
+    if (root == nullptr)
+        return 0;
+
+    if (root->left == nullptr && root->right == nullptr)
+        return 1;
+
+    return countLeafNodes(root->left) +
+           countLeafNodes(root->right);
+}
+
+int main() {
+    TreeNode* root = new TreeNode(1);
+    root->left = new TreeNode(2);
+    root->right = new TreeNode(3);
+    root->left->left = new TreeNode(4);
+    root->left->right = new TreeNode(5);
+
+    cout << "Leaf nodes: " << countLeafNodes(root);
+
+    return 0;
+}
