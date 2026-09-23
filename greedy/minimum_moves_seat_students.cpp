99326f954baf9b0865b368d13c97d1160f2bd69f
Archi Lodha <archilodha1@gmail.com>
2026-09-23 06:12:16 +0530

    Add function to calculate minimum seating moves
    Implement a function to calculate minimum moves required to seat students.

---
+++ greedy/minimum_moves_seat_students.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Minimum moves required to arrange students into seats.
+int minMovesToSeat(vector<int>& seats, vector<int>& students) {
+    sort(seats.begin(), seats.end());
+    sort(students.begin(), students.end());
+
+    int moves = 0;
+
+    for (int i = 0; i < seats.size(); i++) {
+        moves += abs(seats[i] - students[i]);
+    }
+
+    return moves;
+}
